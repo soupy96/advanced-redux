26cc2f171e32f7aad826679263ed147db701444b
Michael Campbell <michael.campbell14@gmail.com>
2022-10-22 18:33:19 -0400

    finishing hooking up the ui and cart slices before moving onto http with redux

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-createSlice({
+const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [],
@@ -10,15 +10,35 @@ createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const exisitingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!exisitingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
+          name: newItem.title,
         });
+      } else {
+        exisitingItem.quantity++;
+        exisitingItem.totalPrice = exisitingItem.totalPrice + newItem.price;
       }
     },
-    removeItemToCart(state) {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const exisitingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (exisitingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        exisitingItem.quantity--;
+        exisitingItem.totalPrice =
+          exisitingItem.totalPrice - exisitingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
