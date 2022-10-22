41cb82b5e4df1191e4e7f39ca989fc2efc3ae42e
Michael Campbell <michael.campbell14@gmail.com>
2022-10-22 17:44:06 -0400

    using redux to create slices for the ui and cart

---
+++ src/store/cart-slice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const exisitingItem = state.items.find((item) => item.id === newItem.id);
+      if (!exisitingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+      }
+    },
+    removeItemToCart(state) {},
+  },
+});
